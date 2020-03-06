89b318e2392b75ea48cbcdcd1cc41121476a50b0
unknown-guy-1610 <unknown-guy-1610@Sandeeps-MacBook-Air.local>
2020-03-06 18:27:16 +0530

    Admin view added

--- core/admin.py
+++ core/admin.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from . models import(
     TeamInfo,
     UserType,
-    evaluator
+    evaluator,
+    Metrix
 )
+
 
 admin.site.register(TeamInfo)
 admin.site.register(UserType)
 admin.site.register(evaluator)
+admin.site.register(Metrix)
