63d8ba4c84f455ba6085a51441f9980ce891c43b
Sandeep <rayanuthalas@gmail.com>
2020-03-10 17:32:35 +0530

    Heroku commit

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku 
+import django_heroku 
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -138,4 +138,4 @@ FCM_DJANGO_SETTINGS = {
 STATIC_URL = '/static/'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
